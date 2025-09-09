0b1f0f05dfab904413f1f9b38ebeba8e11a5748a
EilzDragon92 <71208796+EilzDragon92@users.noreply.github.com>
2025-09-08 22:55:51 -0700

    Create minimum_remove_to_make_valid_parenthesis.cpp

---
+++ LeetCode/minimum_remove_to_make_valid_parenthesis.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/submissions/1764575045
+
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        int size = s.size(), cnt = 0;
+        string res = "";
+
+        for (int i = 0; i < size; i++) {
+            if (s[i] == '(') {
+                cnt++;
+            }
+            else if (s[i] == ')') {
+                if (cnt > 0) cnt--;
+                else s[i] = ' ';
+            }
+        }
+
+        cnt = 0;
+
+        for (int i = size - 1; i >= 0; i--) {
+            if (s[i] == ')') {
+                cnt++;
+            }
+            else if (s[i] == '(') {
+                if (cnt > 0) cnt--;
+                else s[i] = ' ';
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (s[i] != ' ') res += s[i];
+        }
+
+        return res;
+    }
+};
