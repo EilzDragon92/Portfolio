978f93032b27a4c69a470eb990a7f8692ae69ccb
EilzDragon92 <71208796+EilzDragon92@users.noreply.github.com>
2026-01-21 21:33:32 -0800

    Update

---
+++ Projects/AES-GCM/Benchmark/Benchmark.cpp
@@ -0,0 +1,136 @@
+﻿/**
+ * @file    Benchmark.cpp
+ * @brief   AES-GCM and Argon2id peformance test
+ * @author  EilzDragon92
+ */
+
+#include <benchmark/benchmark.h>
+#include "Core/AES_GCM.h"
+#include "Common/header.h"
+
+ /**
+  * @class   Benchmark
+  * @brief   Fixture for AES-GCM and Argon2id performance test
+  */
+class Benchmark : public benchmark::Fixture {
+protected:
+    QString srcPath = "bench_src.tmp";
+    QString encPath = "bench_enc.tmp";
+    QString decPath = "bench_dec.tmp";
+    const char *pw = "password";
+    size_t psize = strlen(pw);
+
+    void create(size_t size) {
+        FILE *file = nullptr;
+
+        OpenFile(&file, srcPath, "wb");
+
+        if (file) {
+            std::vector<uint8_t> data(size, 'a');
+            fwrite(data.data(), 1, size, file);
+            fclose(file);
+        }
+    }
+
+    void clean() {
+        RemoveFile(srcPath);
+        RemoveFile(encPath);
+        RemoveFile(decPath);
+    }
+};
+
+/**
+ * @brief   Encryption benchmark
+ */
+BENCHMARK_DEFINE_F(Benchmark, Encrypt)(benchmark::State &state) {
+    size_t size = state.range(0);
+
+    create(size);
+
+    for (auto _ : state) {
+        AES_GCM aes;
+        FILE *src = nullptr, *dst = nullptr;
+
+        OpenFile(&src, srcPath, "rb");
+        OpenFile(&dst, encPath, "wb+");
+
+        aes.encrypt(src, dst, pw, psize);
+
+        if (src) fclose(src);
+        if (dst) fclose(dst);
+    }
+
+    state.SetBytesProcessed(int64_t(state.iterations()) * fsize);
+    state.SetLabel(std::to_string(fsize / (1024 * 1024)) + " MB");
+
+    clean();
+}
+
+/**
+ * @brief   Decryption benchmark
+ */
+BENCHMARK_DEFINE_F(Benchmark, Decrypt)(benchmark::State &state) {
+    size_t size = state.range(0);
+    create(size);
+
+
+    /* Encrypt */
+
+    AES_GCM aes;
+    FILE *src = nullptr, *dst = nullptr;
+
+    OpenFile(&src, srcPath, "rb");
+    OpenFile(&dst, encPath, "wb+");
+
+    aes.encrypt(src, dst, pw, psize);
+
+    if (src) fclose(src);
+    if (dst) fclose(dst);
+
+
+    /* Decrypt and test performance */
+
+    for (auto _ : state) {
+        AES_GCM aes;
+        FILE *src = nullptr, *dst = nullptr;
+
+        OpenFile(&src, encPath, "rb");
+        OpenFile(&dst, decPath, "wb+");
+
+        aes.decrypt(src, dst, pw, psize);
+
+        if (src) fclose(src);
+        if (dst) fclose(dst);
+    }
+
+    state.SetBytesProcessed(int64_t(state.iterations()) * size);
+
+    clean();
+}
+
+/**
+ * @brief   Argon2id benchmark
+ */
+static void BM_Argon2id(benchmark::State &state) {
+    uint8_t salt[SALT_SIZE], key[KEY_SIZE];
+    const char *pw = "password";
+    int psize = strlen(pw);
+
+    for (int i = 0; i < SALT_SIZE; i++) salt[i] = i;
+
+    for (auto _ : state) Argon2id(salt, pw, psize, key);
+}
+
+BENCHMARK_REGISTER_F(Benchmark, Encrypt)
+->Arg(1024 * 1024 * 1024) 
+->Unit(benchmark::kMillisecond);
+
+BENCHMARK_REGISTER_F(Benchmark, Decrypt)
+->Arg(1024 * 1024 * 1024)
+->Unit(benchmark::kMillisecond);
+
+BENCHMARK(BM_Argon2id)
+->Unit(benchmark::kMillisecond)
+->Iterations(10);
+
+BENCHMARK_MAIN();
